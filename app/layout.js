55f5657f3b5a8604e179b1cdbdc7296fd499baf8
Runjhun saxena <runjhunsaxena6@gmail.com>
2025-09-19 14:38:27 +0530

    Initial setup Shadcn and Next

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+   <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+
+           <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-muted/50 py-12 ">
+            <div className="container mx-auto px-4 text-center text-gray-200 "
+          >
+              <p>Made by Runjhun</p>
+            </div>
+          </footer>
+          </ThemeProvider>
       </body>
     </html>
   );
