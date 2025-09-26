270c0d1a10e9b47202fc5a019ae1eeaa18099be4
Runjhun saxena <runjhunsaxena6@gmail.com>
2025-09-27 00:02:44 +0530

    Industry Insights page Completed

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,17 @@
+import { BarLoader } from "react-spinners";
+import { Suspense } from "react";
+
+export default function Layout({ children }) {
+  return (
+    <div className="px-5">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-gray-400 via-white to-gray-200 mb-2">Industry Insights</h1>
+      </div>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+}
